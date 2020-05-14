784c5ce89601e0463cb97a1d90424bddc319c46f
fjThomasStanley <54365220+fjThomasStanley@users.noreply.github.com>
2020-05-14 09:56:00 +0100

    updated setting.py
    Added STATIC_ROOT

--- oh_app_demo/settings.py
+++ oh_app_demo/settings.py
@@ -9,6 +9,8 @@ import django_heroku
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 # Customization for Heroku-specific settings
 ON_HEROKU = os.getenv('ON_HEROKU', 'false').lower() == 'true'
 
