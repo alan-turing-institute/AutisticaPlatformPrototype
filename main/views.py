01e27355e01c291453d19d416392f3fcb7b97913
pgc007 <64270473+pgc007@users.noreply.github.com>
2020-06-05 08:44:41 +0100

    got coloured text working

--- main/views.py
+++ main/views.py
@@ -48,7 +48,6 @@ def userjourney(request):
         ],
     }
     if request.method == "POST":
-#        print("atcivestepper ", stepper_data['activeStepper'], " nextsetp ", request.POST.get('nextStep'))
         if request.POST.get('nextStep') == 'end':
             nextStep = request.POST.get('nextStep')
             request.session['activeStepper'] = nextStep
