e80d713b0ae4191673cf5c8196b66784d97d4055
George Taylor <george@gpst.me>
2020-07-15 17:11:34 +0100

    rearrange files

--- main_app/settings.py
+++ main_app/settings.py
@@ -11,6 +11,8 @@ from dotenv import load_dotenv
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+load_dotenv(os.path.join(BASE_DIR, '.env'))
+
 # Customization for Heroku-specific settings
 ON_HEROKU = os.getenv('ON_HEROKU', 'false').lower() == 'true'
 
