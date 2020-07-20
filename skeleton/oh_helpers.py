2475737a2e656c05d9e20cfed2077d57de1d6267
George Taylor <george@gpst.me>
2020-07-20 22:13:52 +0100

    create helper functions for oh

---
+++ skeleton/oh_helpers.py
@@ -0,0 +1,29 @@
+import urllib.request, json
+
+class oh_helper:
+
+    def __init__(self, oh_member):
+        self.oh_member = oh_member
+
+    def get_oh_member(self):
+        return self.oh_member
+
+    def set_oh_member(self, oh_member):
+        self.oh_member = oh_member
+
+    def build_experiences_list(self):
+        experiences_array = []
+
+        file_urls = self.oh_member.list_files()
+
+        for file_url in file_urls:
+            with urllib.request.urlopen(file_url) as url:
+                data = json.loads(url.read().decode())
+            if data is not None:
+                # handle json
+
+
+            else:
+                # django error here ideally
+                print("error")
+
