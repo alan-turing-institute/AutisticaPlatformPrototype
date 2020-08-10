1f55e9fd7d975e09084d91ffa84d6c5fd9bae09a
pgc007 <64270473+pgc007@users.noreply.github.com>
2020-08-10 17:30:03 +0100

    Updated index to fix error and include all component data

--- skeleton/views.py
+++ skeleton/views.py
@@ -21,7 +21,7 @@ def index(request):
     Starting page for app.
     """
 
-    test_step_data = {
+    context = {
         "stepper": [
             {
                 "id": 1,
@@ -36,20 +36,121 @@ def index(request):
                 "label": "Add Event"
             }
         ],
+        "ueftext": [
+            {
+                "rows": [
+                    {
+                        "qtext": "Where",
+                        "qcolour": "#4d75ad",
+                        "phtext": "Enter name of location or postcode...",
+                        "input": "ip"
+                    },
+                    {
+                        "qtext": "What",
+                        "qcolour": "#ffbb5d",
+                        "phtext": "Your experience can be entered here...",
+                        "input": "ta"
+                    }
+                ],
+                "maintext": "Enter your experience"
+            },
+            {
+                "rows": [
+                    {
+                        "qtext": "What",
+                        "qcolour": "#ffbb5d",
+                        "phtext": "",
+                        "input": "ta"
+                    }
+                ],
+                "maintext": "What would you have wished to be different?"
+            }
+        ],
+        "user_exp": [
+            {
+                "id": "32097868",
+                "datetime": "Sept 18, 2019, 10:31 a.m.",
+                "user_txt": [
+                    {
+                        "question": "Event",
+                        "text": "The air conditioning in the room where I was having a meeting was really loud and I found it really hard to concentrate."
+                    },
+                    {
+                        "question": "What would you have liked to be different?",
+                        "text": ""
+                    }
+                ]
+            },
+            {
+                "id": "19279611",
+                "datetime": "Sept 17, 2019, 8:46 a.m.",
+                "user_txt": [
+                    {
+                        "question": "Event",
+                        "text": "The tube is too loud."
+                    },
+                    {
+                        "question": "What would you have liked to be different?",
+                        "text": ""
+                    }
+                ]
+            },
+            {
+                "id": "32097868",
+                "datetime": "Sept 17, 2019, 8:45 a.m.",
+                "user_txt": [
+                    {
+                        "question": "Event",
+                        "text": "I'm at a conference today and I found people not using the microphone really difficult - it makes it much harder to concentrate on what they were saying. I was much more distracted."
+                    },
+                    {
+                        "question": "What would you have liked to be different?",
+                        "text": ""
+                    }
+                ]
+            }
+        ],
+        "MONE_data": [
+            {
+                "UID": "0000001",
+                "EID": "32097868",
+                "date": "18/09/19",
+                "Event_What": "The air conditioning in the room where I was having a meeting was really loud and I found it really heard to concentrate, it was a rubbish experience.",
+                "Location_Where": "NW1 2HS",
+                "LikeToBeDifferent": "I would have liked the air conditioning to less loud to aid my concentration",
+                "Summary": "Loud Air Conditioning"
+            },
+            {
+                "UID": "0000002",
+                "EID": "32097867",
+                "date": "17/09/19",
+                "Event_What": "The tube is too loud.",
+                "Location_Where": "NW1 8NH",
+                "LikeToBeDifferent": "would have liked the tube to be less loud",
+                "Summary": "Loud Tube"
+            },
+            {
+                "UID": "0000003",
+                "EID": "32097866",
+                "date": "17/09/19",
+                "Event_What": "I'm at a conference today and I found the people not using the microphone really difficult - it makes it harder to concentrate on what they were saying. I was much more distracted.",
+                "Location_Where": "SE15 5DQ",
+                "LikeToBeDifferent": "For people in conferences to use a microphone. To aid my concentration and reduce my distraction.",
+                "Summary": "None use of microphone in conference"
+            }
+        ]
     }
-    stepper_object = Stepper.Stepper(test_step_data, request)
+    stepper_object = Stepper.Stepper(request)
 
     stepper_object.update()
 
-    stepper_data = stepper_object.get_stepper_data()
-
     auth_url = OpenHumansMember.get_auth_url()
-    context = {'auth_url': auth_url,
-               'oh_proj_page': settings.OH_PROJ_PAGE}
+    context = {**context, **{'auth_url': auth_url,
+                             'oh_proj_page': settings.OH_PROJ_PAGE}}
     if request.user.is_authenticated:
         return redirect('overview')
     # return render(request, 'index.html', context=context)
-    return render(request, 'index.html', stepper_data)
+    return render(request, 'index.html', context=context)
 
 def componentGallery(request):
     context = {
