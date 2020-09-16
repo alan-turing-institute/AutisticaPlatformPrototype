4faf4327f9e4ce6af34e005f3f495a39e6a4b5e3
Daniel Burnett <osw@users.noreply.github.com>
2020-09-16 09:09:40 +0100

    minor update to settings

--- main_app/settings.py
+++ main_app/settings.py
@@ -76,11 +76,11 @@ WSGI_APPLICATION = 'main_app.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
+        'HOST': os.environ.get('POSTGRES_HOST'),
+        'PORT': os.environ.get('POSTGRES_PORT', 5432),
         'NAME': os.environ.get('POSTGRES_DB'),
         'USER': os.environ.get('POSTGRES_USER'),
         'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
-        'HOST': os.environ.get('POSTGRES_HOST'),
-        'PORT': 5432,
     }
 }
 
